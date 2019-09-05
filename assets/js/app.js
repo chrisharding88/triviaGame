33b7bad9d38dc6a0395f6e9bb3c4cd6c9f9df343
Christopher Harding <chrisharding88@gmail.com>
2019-09-05 01:14:26 -0400

    Display the options from the questionBank and print them out on HTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     //Global Variables
     var clockRunning = false;
     var clockId;
+    var currentOptions = 0;
     var shotClock = 24;
     let currentQuestion = 0;
 
@@ -58,16 +59,29 @@ $(document).ready(function(){
    displayQuestion();
 
    function displayOptions(options){
-        let result = '';
-        let choices = $('.options');
+       
 
+       for (var i = 0; i < options.length; i++){
+           var answer1 = $('#ans1');
+           var answer2 = $('#ans2');
+           var answer3 = $('#ans3');
+           var answer4 = $('#ans4');
+           
+           if (i===0){
+               answer1.attr('data-answer', options[i]);
+           } else if (i===1){
+               answer2.attr('data-answer', options[i]);
+           } else if (i===2){
+               answer3.attr('data-answer', options[i]);
+           } else if (i===3){
+              answer4.attr('data-answer', options[i]);
+           }
 
-       for (let i = 0; i < options.length; i++){
-           result += choices;
-           choices.attr('data-answer', options[i]);
+           var choices = questionBank[currentOptions].options[i];
+           $('.answers').append('<p>' +'<button type = "button" class="btn btn-primary">' + choices + '</button>' + '</p>');
+
        }
 
-       return result;
 
    }
 
